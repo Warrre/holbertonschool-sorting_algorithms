43bfc8253bd832bc5025782348f4b9fc714e5174
WassefRPZ <10422@holbertonstudents.com>
2025-04-11 22:51:42 +0200

    Fix indentation for consistency in quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,9 +7,9 @@
  */
 void swap(int *a, int *b)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
+	int t = *a;
+	*a = *b;
+	*b = t;
 }
 
 /**
@@ -22,38 +22,38 @@ void swap(int *a, int *b)
  */
 int partition(int arr[], size_t low, size_t high, size_t size)
 {
-    int pivot = arr[high];
-    size_t i = low - 1;
-    size_t j;
+	int pivot = arr[high];
+	size_t i = low - 1;
+	size_t j;
 
-    if (arr == NULL || size < 2)
-    {
-        return (-1);
-    }
+	if (arr == NULL || size < 2)
+	{
+		return (-1);
+	}
 
-    if (high >= size || low > high)
-    {
-        return (-1);
-    }
+	if (high >= size || low > high)
+	{
+		return (-1);
+	}
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (arr[j] <= pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                swap(&arr[i], &arr[j]);
-                print_array(arr, size);
-            }
-        }
-    }
-    if (i + 1 != high)
-    {
-        swap(&arr[i + 1], &arr[high]);
-        print_array(arr, size);
-    }
-    return (i + 1);
+	for (j = low; j <= high - 1; j++)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&arr[i], &arr[j]);
+				print_array(arr, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		swap(&arr[i + 1], &arr[high]);
+		print_array(arr, size);
+	}
+	return (i + 1);
 }
 
 /**
@@ -65,18 +65,18 @@ int partition(int arr[], size_t low, size_t high, size_t size)
  */
 void helper(int arr[], int low, int high, size_t size)
 {
-    if (arr == NULL || size < 2)
-    {
-        return;
-    }
+	if (arr == NULL || size < 2)
+	{
+		return;
+	}
 
-    if (low < high)
-    {
-        int pi = partition(arr, low, high, size);
+	if (low < high)
+	{
+		int pi = partition(arr, low, high, size);
 
-        helper(arr, low, pi - 1, size);
-        helper(arr, pi + 1, high, size);
-    }
+		helper(arr, low, pi - 1, size);
+		helper(arr, pi + 1, high, size);
+	}
 }
 
 /**
@@ -87,10 +87,10 @@ void helper(int arr[], int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size <= 1)
-    {
-        return;
-    }
+	if (array == NULL || size <= 1)
+	{
+		return;
+	}
 
-    helper(array, 0, size - 1, size);
+	helper(array, 0, size - 1, size);
 }
