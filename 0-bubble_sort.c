eec3b203514930331bb3ef222fbeb5da2ebee8e6
WassefRPZ <10422@holbertonstudents.com>
2025-04-11 20:17:29 +0200

    Refactor bubble_sort for consistent indentation and formatting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,26 +11,26 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    int temp;
-    int swapped = 1;
+	size_t i;
+	int temp;
+	int swapped = 1;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (swapped == 1)
-    {
-        swapped = 0;
-        for (i = 0; i < size - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
-    }
+	while (swapped == 1)
+	{
+		swapped = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+	}
 }
