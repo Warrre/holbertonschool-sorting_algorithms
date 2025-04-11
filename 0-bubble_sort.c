3c95afb7c6588136a115cb8750c1b0c19ed65243
warrre <warren.soulsey@gmail.com>
2025-04-11 18:17:58 +0200

    Refactor bubble_sort function for clarity and efficiency

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,36 +4,29 @@
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order
- *               using the Bubble sort algorithm
- *
  * @array: The array to be sorted
- * @size: The number of elements in the array
- *
- * Description: Prints the array after each swap of two elements.
+ * @size: Number of elements in @array
  */
-void bubble_sort(int *array, size_t size)
-{
-	size_t i;
-	int temp;
-	int swapped;
 
-	if (array == NULL || size < 2)
-		return;
-
-	swapped = 1;
-	while (swapped == 1)
-	{
-		swapped = 0;
-		for (i = 0; i < size - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				swapped = 1;
-				print_array(array, size);
-			}
-		}
-	}
-}
+ void bubble_sort(int *array, size_t size)
+ {
+     size_t i, j;
+     int temp;
+ 
+     if (array == NULL || size < 2)
+         return;
+ 
+     for (i = 0; i < size - 1; i++)
+     {
+         for (j = 0; j < size - i - 1; j++)
+         {
+             if (array[j] > array[j + 1])
+             {
+                 temp = array[j];
+                 array[j] = array[j + 1];
+                 array[j + 1] = temp;
+                 print_array(array, size);
+             }
+         }
+     }
+ }
